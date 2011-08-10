473707e9518b7907d76ac06766c71a5c35eca002
thindil <thindil@tuxfamily.org>
2011-08-10 11:29:16 +0200

    automation of labirynth

--- grid.php
+++ grid.php
@@ -4,10 +4,10 @@
  *   Labyrynth - explore and quests
  *
  *   @name                 : grid.php                            
- *   @copyright            : (C) 2004,2005,2006 Vallheru Team based on Gamers-Fusion ver 2.5
- *   @author               : thindil <thindil@users.sourceforge.net>
- *   @version              : 1.2
- *   @since                : 24.07.2006
+ *   @copyright            : (C) 2004,2005,2006,2011 Vallheru Team based on Gamers-Fusion ver 2.5
+ *   @author               : thindil <thindil@tuxfamily.org>
+ *   @version              : 1.4
+ *   @since                : 10.08.2011
  *
  */
  
@@ -27,7 +27,7 @@
 //   along with this program; if not, write to the Free Software
 //   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
-// $Id: grid.php 520 2006-07-24 10:19:05Z thindil $
+// $Id$
 
 $title = "Labirynt";
 require_once("includes/head.php");
@@ -45,16 +45,41 @@ if ($player -> location != 'Altara' && $player -> location != 'Podróż')
 $smarty -> assign(array("Chance" => '', 
                         "Maps" => ''));
 
-$query = $db -> Execute("SELECT quest FROM questaction WHERE player=".$player -> id." AND action!='end'");
+$query = $db -> Execute("SELECT `quest` FROM `questaction` WHERE `player`=".$player -> id." AND `action`!='end'");
+// Enter to labirynth
 if (!isset($_GET['action']) && !isset($_GET['step']) && empty($query -> fields['quest']))
-{
+  {
     $smarty -> assign(array("Labinfo" => LAB_INFO,
-                            "Ayes" => YES));
-}
+			    "Explore" => A_EXP,
+			    "Times" => "razy"));
+  }
 
+$strQuestName = "";
+
+//Explore labirynth
 if (isset ($_GET['action']) && $_GET['action'] == 'explore' && empty($query -> fields['quest'])) 
-{
-    if ($player -> energy < .3) 
+  {
+
+    function findmap()
+    {
+      global $db;
+      global $player;
+      $objMaps = $db -> Execute("SELECT `value` FROM `settings` WHERE `setting`='maps'");
+      $roll = rand(1,50);
+      if ($roll == 50 && $objMaps -> fields['value'] > 0 && $player -> maps < 20 && $player -> rank != 'Bohater') 
+	{
+	  $objMaps -> Close();
+	  return 1;
+	}
+      $objMaps -> Close();
+      return 0;
+    }
+
+    integercheck($_POST['amount']);
+    checkvalue($_POST['amount']);
+    $intAmount = intval($_POST['amount']);
+    $intNeeded = $intAmount * 0.3;
+    if ($player -> energy < $intNeeded) 
     {
         error (NO_ENERGY);
     }
@@ -62,145 +87,142 @@ if (isset ($_GET['action']) && $_GET['action'] == 'explore' && empty($query -> f
     {
         error (YOU_DEAD);
     }
-    $chance = rand(1,11);
-    $db -> Execute("UPDATE players SET energy=energy-.3 WHERE id=".$player -> id);
-    if ($chance == 1)
-    {
-        $smarty -> assign("Action2", ACTION1);
-    }
-    if ($chance == 2)
-    {
-        $smarty -> assign("Action2", ACTION2);
-    }
-    if ($chance == 3) 
-    {
-        $crgain = rand(1,100);
-        $smarty -> assign (array("Goldgain" => $crgain,
-                                 "Action2" => ACTION3,
-                                 "Action3" => ACTION3_1));
-        $db -> Execute("UPDATE players SET credits=credits+".$crgain." WHERE id=".$player -> id);
-    }
-    if ($chance == 4)
-    {
-        $smarty -> assign("Action2", ACTION4);
-    }
-    if ($chance == 5)
-    {
-        $smarty -> assign("Action2", ACTION5);
-    }
-    if ($chance == 6) 
-    {
-        $plgain = rand(1,3);
-        $smarty -> assign (array("Mithgain" => $plgain,
-                                 "Action2" => ACTION6,
-                                 "Action3" => ACTION6_1));
-        $db -> Execute("UPDATE players SET platinum=platinum+".$plgain." WHERE id=".$player -> id);
-    }
-    if ($chance == 7) 
-    {
-        $smarty -> assign("Action2", ACTION7_1);
-        $db -> Execute("UPDATE players SET energy=energy+1 WHERE id=".$player -> id);
-    }
-    if ($chance == 8)
-    {
-        $smarty -> assign("Action2", ACTION8);
-    }
-    if ($chance == 10) 
-    {
-        $intRoll = rand(1, 5);
-        if ($intRoll == 5)
-        {
-            $aviable = $db -> Execute("SELECT `qid` FROM `quests` WHERE `location`='grid.php' AND `name`='start'");
-            $number = $aviable -> RecordCount();
-            if ($number > 0) 
-            {
-                $arramount = array();
-                $i = 0;
-                while (!$aviable -> EOF) 
-                {
-                    $query = $db -> Execute("SELECT `id` FROM `questaction` WHERE `quest`=".$aviable -> fields['qid']." AND `player`=".$player -> id);
-                    if (empty($query -> fields['id'])) 
-                    {
-                        $arramount[$i] = $aviable -> fields['qid'];
-                        $i = $i + 1;
-                    }
-                    $query -> Close();
-                    $aviable -> MoveNext();
-                }
-                $i = $i - 1;
-                if ($i >= 0) 
-                {
-                    $roll = rand(0,$i);
-                    $name = "quest".$arramount[$roll].".php";
-                    require_once('includes/statsbonus.php');
-                    $arrCurstats = statbonus();
-                    require_once("quests/".$name);
-                } 
+    $intGold = 0;
+    $intMithril = 0;
+    $intEnergy = 0;
+    $intTimes = 0;
+    $finish = FALSE;
+    $arrAstral = array();
+    $intMaps = 0;
+    while (!$finish)
+      {
+	$chance = rand(1, 11);
+	switch ($chance)
+	  {
+	  case 3:
+	    $intGold += rand(1, 100);
+	    break;
+	  case 6:
+	    $intMithril += rand(1, 3);
+	    break;
+	  case 7:
+	    $intEnergy++;
+	    break;
+	  case 10:
+	    $intRoll = rand(1, 5);
+	    if ($intRoll == 5)
+	      {
+		$available = $db -> Execute("SELECT `qid` FROM `quests` WHERE `location`='grid.php' AND `name`='start'");
+		$number = $available -> RecordCount();
+		if ($number > 0) 
+		  {
+		    $arramount = array();
+		    $i = 0;
+		    while (!$available -> EOF) 
+		      {
+			$query = $db -> Execute("SELECT `id` FROM `questaction` WHERE `quest`=".$available -> fields['qid']." AND `player`=".$player -> id);
+			if (empty($query -> fields['id'])) 
+			  {
+			    $arramount[$i] = $available -> fields['qid'];
+			    $i = $i + 1;
+			  }
+			$query -> Close();
+			$available -> MoveNext();
+		      }
+		    $i = $i - 1;
+		    if ($i >= 0) 
+		      {
+			$roll = rand(0,$i);
+			$strQuestName = "quest".$arramount[$roll].".php";
+			$finish = TRUE;
+		      } 
                     else 
-                {
-                    $chance = 9;
-                }
-            }
-            $aviable -> Close();
-        }
+		      {
+			$intMaps += findmap();
+		      } 
+		  }
+		$available -> Close();
+	      }
+	    break;
+	  case 11:
+	    require_once('includes/findastral.php');
+	    $strResult = findastral(5);
+	    if ($strResult != false)
+	      {
+		$arrAstral[] = $strResult;
+	      }
             else
-        {
-            $chance = 9;
-        }
-    }
-    /**
-     * Find astral components
-     */
-    if ($chance == 11)
-    {
-        require_once('includes/findastral.php');
-        $strResult = findastral(5);
-        if ($strResult != false)
-        {
-            $smarty -> assign("Action2", ACTION11.$strResult);
-        }
-            else
-        {
-            $chance = 9;
-        }
-    }
-    if ($chance == 9) 
-    {
-        $objMaps = $db -> Execute("SELECT value FROM settings WHERE setting='maps'");
-        $roll = rand(1,50);
-        if ($roll == 50 && $objMaps -> fields['value'] > 0 && $player -> maps < 20 && $player -> rank != 'Bohater') 
-        {
-            $text = CHEST;
-            if ($player -> clas == 'Wojownik' || $player -> clas == 'Barbarzyńca') 
-            {
-                $text= $text.CHEST2;
-            }
-            if ($player -> clas == 'Mag') 
-            {
-                $text = $text.CHEST3;
-            }
-            if ($player -> clas == 'Obywatel' || $player -> clas == 'Złodziej' || $player -> clas == '') 
-            {
-                $text = $text.CHEST4;
-            }
-            $text = $text.CHEST5;
-            $smarty -> assign ("Action2", $text);
-            $db -> Execute("UPDATE players SET maps=maps+1 WHERE id=".$player -> id);
-            $intMaps = $objMaps -> fields['value'] - 1;
-            $db -> Execute("UPDATE settings SET value='".$intMaps."' WHERE setting='maps'");
-        }
-            else
-        {
-            $smarty -> assign("Action2", ACTION9);
-        }
-        $objMaps -> Close();
-    }
-    $energyleft = ($player -> energy - .3);
-    $smarty -> assign(array("Chance" => $chance, 
-                            "Energyleft" => $energyleft,
-                            "Aexp" => A_EXP,
-                            "Tnext" => T_NEXT,
-                            "Enpts" => EN_PTS));
+	      {
+		$intMaps += findmap();
+	      }
+	    break;
+	  case 9:
+	    $intMaps += findmap();
+	    break;
+	  default:
+	    break;
+	  }
+	$intTimes ++;
+	if ($intTimes == $intAmount)
+	  {
+	    $finish = TRUE;
+	  }
+      }
+    //Count what we found
+    if ($player->gender == 'F')
+      {
+	$strLast = "aś";
+      }
+    else
+      {
+	$strLast = "eś";
+      }
+    
+    if (($intGold > 0) || ($intMithril > 0) || ($intEnergy > 0) || ($intMaps > 0) || (count($arrAstral) > 0))
+      {
+	$strText = "Podczas swojej wędrówki znalazł".$strLast.":<br />";
+      }
+    else
+      {
+	$strText = "Wędrował".$strLast." jakiś czas ale nic ciekawego nie znalazł".$strLast.".<br />";
+      }
+    if ($intGold > 0)
+      {
+	$strText .= $intGold." sztuk złota<br />";
+      }
+    if ($intMithril > 0)
+      {
+	$strText .= $intMithril." sztuk mithrilu<br />";
+      }
+    if ($intEnergy > 0)
+      {
+	$strText .= $intEnergy." razy źródełko<br />";
+      }
+    if ($intMaps > 0)
+      {
+	$strText .= $intMaps." kawałków mapy<br />";
+	$objMaps = $db -> Execute("SELECT `value` FROM `settings` WHERE `setting`='maps'");
+	$intMaps2 = $objMaps -> fields['value'] - $intMaps;
+	$db->Execute("UPDATE `settings` SET `value`='".$intMaps2."' WHERE `setting`='maps'");
+      }
+    foreach ($arrAstral as $strAstral)
+      {
+	$strText .= $strAstral."<br />";
+      }
+    $intTimes = $intTimes * 0.3;
+    $strText .= "Zużył".$strLast." na to ".$intTimes." energii.<br />";
+    if ($strQuestName == '')
+      {
+	$strBack = "Wróć";
+      }
+    else
+      {
+	$strBack = "";
+      }
+    $smarty->assign(array("Action2" => $strText,
+			  "Back" => $strBack));
+    $intEnergy -= $intTimes;
+    $db->Execute("UPDATE `players` SET `credits`=`credits`+".$intGold.", `platinum`=`platinum`+".$intMithril.", `energy`=`energy`+".$intEnergy.", `maps`=`maps`+".$intMaps." WHERE `id`=".$player->id);
 }
 
 if ((isset($_GET['step']) && $_GET['step'] == 'quest') || !empty($query -> fields['quest'])) 
@@ -243,5 +265,12 @@ $smarty -> assign(array("Action" => $_GET['action'],
 $smarty -> display ('grid.tpl');
 
 $query -> Close();
+
+if ($strQuestName != "")
+  {
+    require_once('includes/statsbonus.php');
+    $arrCurstats = statbonus();
+    require_once("quests/".$strQuestName);
+  }
 require_once("includes/foot.php");
 ?>
--- templates/grid.tpl
+++ templates/grid.tpl
@@ -1,20 +1,16 @@
 {if $Action == "" && $Step == "" && $Quest == 'N'}
-    {$Labinfo} <a href="grid.php?action=explore">{$Ayes}.</a>
+    {$Labinfo}<br /><br />
+    <form method="post" action="grid.php?action=explore">
+        <input type="submit" value="{$Explore}" /> <input type="text" name="amount" value="1" size="5" /> {$Times}.
+    </form
 {/if}
 
-{if ($Chance == "1" || $Chance == "2" || $Chance == "4" || $Chance == "5" || $Chance == "7" || $Chance == "8" || $Chance == "9" || $Chance == 11) && $Quest == 'N'}
+{if $Action == "explore" && $Quest == 'N'}
     {$Action2}
-{/if}
-
-{if $Chance == "3" && $Quest == 'N'}
-    {$Action2} <b>{$Goldgain}</b> {$Action3}
-{/if}
-
-{if $Chance == "6" && $Quest == 'N'}
-    {$Action2} <b>{$Mithgain}</b> {$Action3}
-{/if}
-
-{if $Action == "explore" && ($Chance < 10 || $Chance == 11) && $Quest == 'N'}
-    <br /><br />... <a href="grid.php?action=explore">{$Aexp}</a> {$Tnext} {$Energyleft} {$Enpts}.)
+    {if $Back != ""}
+        <a href="grid.php">{$Back}</a>
+    {else}
+        <br />
+    {/if}
 {/if}
 
