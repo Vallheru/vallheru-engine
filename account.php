88afcc1290de0ae276c5a1fe4a6ab8f9e8a35cba
thindil <thindil@tuxfamily.org>
2011-08-20 12:25:04 +0200

    fixed typo

--- account.php
+++ account.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2007,2011 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@tuxfamily.org>
  *   @version              : 1.4
- *   @since                : 18.08.2011
+ *   @since                : 20.08.2011
  *
  */
 
@@ -68,7 +68,7 @@ if (isset($_GET['view']) && $_GET['view'] == 'links')
     }
         else
     {
-       $_GET['lid'] = intvalue($_GET['lid']);
+       $_GET['lid'] = intval($_GET['lid']);
        if ($_GET['lid'] == 0)
        {
            $strFormaction = A_ADD;
