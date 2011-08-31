36cb1bad19f024c7929b5baa47bd923638bc655f
thindil <thindil@tuxfamily.org>
2011-08-31 18:31:23 +0200

    show price for one arrow too

--- bows.php
+++ bows.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2011 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@tuxfamily.org>
  *   @version              : 1.4
- *   @since                : 28.08.2011
+ *   @since                : 31.08.2011
  *
  */
 
@@ -64,7 +64,14 @@ if (!isset ($_GET['buy']) && !isset($_GET['step']))
         $arrspeed[$i] = $wep -> fields['szyb'];
         $arrdur[$i] = $wep -> fields['maxwt'];
         $arrlevel[$i] = $wep -> fields['minlev'];
-        $arrcost[$i] = $wep -> fields['cost'];
+	if ($wep->fields['type'] == 'R')
+	  {
+	    $arrcost[$i] = $wep->fields['cost']."/".ceil($wep->fields['cost'] / 20);
+	  }
+	else
+	  {
+	    $arrcost[$i] = $wep->fields['cost'];
+	  }
         $arrid[$i] = $wep -> fields['id'];
         if ($wep -> fields['type'] == 'R')
         {
