d39d411487b28e40d0ac1cbb709a688250f9ff44
thindil <thindil@tuxfamily.org>
2011-09-04 18:10:44 +0200

    fixed bug with bot answers

--- class/bot_class.php
+++ class/bot_class.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2011 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@tuxfamily.org>
  *   @version              : 1.4
- *   @since                : 08.08.2011
+ *   @since                : 04.09.2011
  *
  */
 
@@ -167,6 +167,7 @@ class Bot
             $strReg = str_replace('.', '', $strReg);
             $strReg = str_replace('*', '', $strReg);
             $strReg = strtolower($strReg);
+	    $this->strPlayertext = str_replace($this->strBotname, '', $this->strPlayertext);
             $this -> strPlayertext = strtolower($this -> strPlayertext);
             $strItem = str_replace($strReg, '', $this -> strPlayertext);
             if (in_array($strItem, $this -> arrReflorg))
