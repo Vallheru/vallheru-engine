647e705f30cc3220315ebf1f89b6578a0efb942a
thindil <thindil@tuxfamily.org>
2011-09-09 19:52:08 +0200

    fixed bug with adding potions to offert

--- market.php
+++ market.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2011 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@tuxfamily.org>
  *   @version              : 1.4
- *   @since                : 29.08.2011
+ *   @since                : 09.09.2011
  *
  */
 
@@ -512,7 +512,7 @@ if (isset($_GET['view']) && $_GET['view'] == 'myoferts')
                     }
                     if ($intKey == 2)
                     {
-                        $objAmount = $db -> Execute("SELECT `id`, `amount` FROM `potions` WHERE `owner`=".$player -> id." AND `name`='".$objOfert -> fields['name']."' AND `efect`='".$objOfert -> fields['efect']."' AND `type`='".$objOfert -> fields['type']."'");
+                        $objAmount = $db -> Execute("SELECT `id`, `amount` FROM `potions` WHERE `owner`=".$player -> id." AND `name`='".$objOfert -> fields['name']."' AND `efect`='".$objOfert -> fields['efect']."' AND `type`='".$objOfert -> fields['type']."' AND `power`=".$objOfert->fields['power']);
                         $intAmount = $objAmount -> fields['amount'];
                     }
                     if ($intKey == 3)
