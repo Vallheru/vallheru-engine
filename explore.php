34c74f3b5e8f10c372f99d73e6622f099022f347
thindil <thindil@tuxfamily.org>
2011-09-12 10:38:42 +0200

    added info about gained perception

--- explore.php
+++ explore.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2011 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@tuxfamily.org>
  *   @version              : 1.4
- *   @since                : 11.09.2011
+ *   @since                : 12.09.2011
  *
  */
  
@@ -191,7 +191,7 @@ if (isset($_GET['step']) && $_GET['step'] == 'run')
                                 "Expgain" => $expgain,
                                 "Escapesucc" => ESCAPE_SUCC,
                                 "Escapesucc2" => ESCAPE_SUCC2,
-                                "Escapesucc3" => ESCAPE_SUCC3));
+                                "Escapesucc3" => ESCAPE_SUCC3." oraz ".$fltPerception." do umiejętności Spostrzegawczość."));
         checkexp($player -> exp, $expgain, $player -> level, $player -> race, $player -> user, $player -> id, 0, 0, $player -> id, 'perception', $fltPerception);
         $db -> Execute("UPDATE `players` SET `fight`=0 WHERE `id`=".$player -> id);
     } 
