10f9222d063cdb81dbf5213532b2852eaff2a268
thindil <thindil@tuxfamily.org>
2011-10-02 10:29:26 +0200

    fixed bug with no monster to escape

--- explore.php
+++ explore.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2011 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@tuxfamily.org>
  *   @version              : 1.4
- *   @since                : 26.09.2011
+ *   @since                : 02.10.2011
  *
  */
  
@@ -160,6 +160,10 @@ if (isset($_GET['step']) && $_GET['step'] == 'battle')
 if (isset($_GET['step']) && $_GET['step'] == 'run') 
 {
     $enemy = $db -> Execute("SELECT `level`, `speed`, `name`, `exp1`, `exp2`, `id` FROM monsters WHERE id=".$player -> fight);
+    if (!$enemy->fields['id'])
+      {
+	error('Nie masz przed kim uciekać!');
+      }
     /**
      * Add bonus from rings
      */
