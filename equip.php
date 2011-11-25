ee33ba9031aff1701c3c5f2d65cc2c13b05bcd3c
thindil <thindil@tuxfamily.org>
2011-11-25 11:23:06 +0100

    fixed notice

--- equip.php
+++ equip.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2007,2011 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@tuxfamily.org>
  *   @version              : 1.4
- *   @since                : 21.11.2011
+ *   @since                : 25.11.2011
  *
  */
 
@@ -1154,6 +1154,10 @@ if (isset($_GET['drinkfew']))
 			  "Pname" => $objPotion->fields['name']));
     if (isset($_GET['step']) && $_GET['step'] == 'drink')
       {
+	if (!isset($_POST['amount']))
+	  {
+	    error("Podaj ile napojów chcesz wypić.");
+	  }
 	checkvalue($_POST['amount']);
 	if ($_POST['amount'] > $objPotion->fields['amount'])
 	  {
