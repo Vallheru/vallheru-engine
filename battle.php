52af8940db2d6b906ded7e0797403293813fbc22
thindil <thindil@tuxfamily.org>
2011-12-03 10:51:56 +0100

    raised limit of fast fights

--- battle.php
+++ battle.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2011 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@tuxfamily.org>
  *   @version              : 1.4
- *   @since                : 07.11.2011
+ *   @since                : 03.12.2011
  *
  */
 
@@ -559,7 +559,7 @@ if (isset($_GET['action']))
 	      {
 		error(TOO_MUCH_MONSTERS);
 	      }
-	    if ($_POST['times'] > 10)
+	    if ($_POST['times'] > 20)
 	      {
 		error("Zbyt wiele walk na raz!");
 	      }
