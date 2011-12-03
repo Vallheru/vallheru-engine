9fb530e47a7a2bd6b44a1ada8220cc7f4fbb5b5b
thindil <thindil@tuxfamily.org>
2011-12-03 14:20:05 +0100

    fixed bug with gaining train points

--- includes/resets.php
+++ includes/resets.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2007,2011 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@tuxfamily.org>
  *   @version              : 1.4
- *   @since                : 28.11.2011
+ *   @since                : 03.12.2011
  *
  */
 
@@ -364,6 +364,7 @@ function mainreset()
     $db -> Execute("UPDATE `settings` SET `value`='Wykonywanie resetu' WHERE `setting`='close_reason'");
     $db -> Execute("UPDATE `players` SET `age`=`age`+1, `hp`=`max_hp`, `bridge`='N', `houserest`='N'");
     $db->Execute("UPDATE `players` SET `newbie`=`newbie`-1 WHERE `newbie`>0");
+    $db->Execute("UPDATE `players` SET `trains`=`trains`+15 WHERE `corepass`='Y' AND `freeze`=0");
     $intCtime = (time() - 200);
     $db -> Execute("UPDATE players SET freeze=freeze-1, lpv=".$intCtime." WHERE freeze>0");
     /**
