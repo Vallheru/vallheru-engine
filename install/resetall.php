adf51899d6745b9700c3f9c306438a73adf6a001
thindil <thindil@tuxfamily.org>
2011-12-19 12:33:41 +0100

    clear table brecords too

--- install/resetall.php
+++ install/resetall.php
@@ -5,9 +5,9 @@
  *
  *   @name                 : resetall.php                            
  *   @copyright            : (C) 2004,2005,2006,2007,2011 Vallheru Team based on Gamers-Fusion ver 2.5
- *   @author               : thindil <thindil@tuxfamily.org>
- *   @version              : 1.4
- *   @since                : 28.11.2011
+ *   @author               : thindil <thindil@vallheru.net>
+ *   @version              : 1.5
+ *   @since                : 19.12.2011
  *
  */
 
@@ -97,6 +97,7 @@ $db -> Execute("TRUNCATE TABLE `amarket`");
 $db -> Execute("TRUNCATE TABLE `astral_plans`");
 $db -> Execute("TRUNCATE TABLE `astral`");
 $db->Execute("TRUNCATE TABLE `revent`");
+$db->Execute("TRUNCATE TABLE `brecords`");
 print "Tables cleared<br />";
 
 /**
