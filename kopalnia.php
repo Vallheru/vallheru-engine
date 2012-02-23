8e7b4502e4d80fb5990fca57a2ffe1c4fcaaa349
thindil <thindil@tuxfamily.org>
2012-02-23 10:35:56 +0100

    fixed info about death in mine

--- kopalnia.php
+++ kopalnia.php
@@ -4,10 +4,10 @@
  *   Mines in moutains
  *
  *   @name                 : kopalnia.php                            
- *   @copyright            : (C) 2004,2005,2006,2011 Vallheru Team based on Gamers-Fusion ver 2.5
- *   @author               : thindil <thindil@tuxfamily.org>
- *   @version              : 1.4
- *   @since                : 07.11.2011
+ *   @copyright            : (C) 2004,2005,2006,2011,2012 Vallheru Team based on Gamers-Fusion ver 2.5
+ *   @author               : thindil <thindil@vallheru.net>
+ *   @version              : 1.5
+ *   @since                : 23.02.2012
  *
  */
 
@@ -35,7 +35,7 @@ require_once("includes/head.php");
 /**
 * Get the localization for game
 */
-require_once("languages/".$player -> lang."/kopalnia.php");
+require_once("languages/".$lang."/kopalnia.php");
 
 if ($player -> location != 'Góry') 
 {
@@ -134,7 +134,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'dig')
                 $strInfo = M_DEAD;
 		if ($player->antidote == 'R')
 		  {
-		    $strInfo = ' na szczęście w tym wypadku, udało ci się oszukać przeznaczenie.';
+		    $strInfo .= ' na szczęście w tym wypadku, udało ci się oszukać przeznaczenie.';
 		    $player->hp = 1;
 		    $db->Execute("UPDATE `players` SET `antidote`='' WHERE `id`=".$player->id);
 		  }
