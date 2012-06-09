ea7b84a95dc4218b50156fd610c00197020bc362
thindil <thindil@tuxfamily.org>
2012-06-09 10:43:06 +0200

    changed stats of guardian

--- portal.php
+++ portal.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2011,2012 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@vallheru.net>
  *   @version              : 1.6
- *   @since                : 06.06.2012
+ *   @since                : 09.06.2012
  *
  */
  
@@ -78,12 +78,12 @@ if (isset ($_GET['action1']) && $_GET['action1'] == 'fight' && $player -> hp > 0
         $smarty -> assign ("Message", START_FIGHT);
         $smarty -> display ('error1.tpl');
         $enemy = array ('name' => MONSTER_NAME, 
-                        'strength' => 1500, 
-                        'agility' => 1500, 
+                        'strength' => 5000, 
+                        'agility' => 5000, 
                         'hp' => 50000, 
                         'level' => 1, 
-                        'endurance' => 1500, 
-                        'speed' => 1500,
+                        'endurance' => 5000, 
+                        'speed' => 5000,
 			'lootnames' => array(),
 			'lootchance' => array(),
 			'exp1' => 0,
