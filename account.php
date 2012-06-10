59943e68bfea3453a02b5364b86283192b22fc42
thindil <thindil@tuxfamily.org>
2012-06-10 10:10:09 +0200

    fixed notice

--- account.php
+++ account.php
@@ -6,8 +6,8 @@
  *   @name                 : account.php                            
  *   @copyright            : (C) 2004,2005,2006,2007,2011,2012 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@vallheru.net>
- *   @version              : 1.5
- *   @since                : 23.05.2012
+ *   @version              : 1.6
+ *   @since                : 10.06.2012
  *
  */
 
@@ -983,7 +983,6 @@ if (isset($_GET['view']))
 		$arrChecked[] = '';
 	      }
 	    $cat -> MoveNext();
-	    $i ++;
 	  }
 	$cat -> Close();
 	if ($player->settings['forumcats'] == 'All')
