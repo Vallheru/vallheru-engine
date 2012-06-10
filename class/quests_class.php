543ef27fa62a8d43e237f58daa99f80ba99b65fb
thindil <thindil@tuxfamily.org>
2012-06-10 02:51:12 +0200

    fixed lang variable

--- class/quests_class.php
+++ class/quests_class.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2011,2012 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@vallheru.net>
  *   @version              : 1.6
- *   @since                : 06.06.2012
+ *   @since                : 10.06.2012
  *
  */
 
@@ -236,6 +236,7 @@ class Quests
         global $db;
         global $smarty;
         global $player;
+	global $lang;
         $gainexp = $exp * $player -> level;
         $text = Q_YOU_GAIN." ".$gainexp." ".Q_EXPERIENCE;
         require_once("includes/checkexp.php");
