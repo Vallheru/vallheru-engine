a9b8f53b5f80a00ebe7eccc8e108f149d8c0ef9f
thindil <thindil@tuxfamily.org>
2012-06-22 14:18:04 +0200

    a bit better check for bad words

--- includes/bbcode.php
+++ includes/bbcode.php
@@ -6,8 +6,8 @@
  *   @name                 : bbcode.php                            
  *   @copyright            : (C) 2004,2005,2006,2011,2012 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@vallheru.net>
- *   @version              : 1.5
- *   @since                : 02.04.2012
+ *   @version              : 1.6
+ *   @since                : 22.06.2012
  *
  */
 
@@ -44,7 +44,7 @@ function bbcodetohtml($text, $isChat = FALSE)
     $arrWords = array();
     foreach ($arrBadwords as $arrBadword)
       {
-	$arrWords[] = '/\b'.$arrBadword['bword'].'\b/'; 
+	$arrWords[] = '/\b'.$arrBadword['bword'].'\b/i'; 
       }
     $text = preg_replace($arrWords, '[kwiatek]', $text);
 
