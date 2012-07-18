82f2113fad026a3d1a2ee1ae5828e19cf7dd77f8
thindil <thindil@tuxfamily.org>
2012-07-18 10:28:31 +0200

    raised chance for gain loot

--- includes/funkcje.php
+++ includes/funkcje.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2007,2011,2012 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@vallheru.net>
  *   @version              : 1.6
- *   @since                : 22.06.2012
+ *   @since                : 18.07.2012
  *
  */
 
@@ -78,7 +78,7 @@ function monsterloot($arrNames, $arrChances, $intLevel, $intMonsters = 1)
   for ($i = 0; $i < $intMonsters; $i++)
     {
       $fltChance = (float)100 / $intLevel;
-      $fltRandom = (float)rand(0, 10000) / 100;
+      $fltRandom = (float)rand(0, 5000) / 100;
       
       //Bad luck, exit
       if ($fltRandom > $fltChance)
