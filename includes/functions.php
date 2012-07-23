b2e1ccaee6f1095461e9b6c812f35a6a55cbaaca
thindil <thindil@tuxfamily.org>
2012-07-23 11:32:00 +0200

    fixed bug with equip arrows

--- includes/functions.php
+++ includes/functions.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2011,2012 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@vallheru.net>
  *   @version              : 1.6
- *   @since                : 20.06.2012
+ *   @since                : 23.07.2012
  *
  */
 
@@ -334,7 +334,7 @@ function equip ($id)
 	//Take off old arrows
 	if ($arrows->fields['id'])
 	  {
-	    $test = $db -> Execute("SELECT id FROM equipment WHERE name='".$arrows->fields['name']."' AND status='U' AND owner=".$player->id." AND power=".$arrows->fields['power']." AND poison=".$arrows->fields['poison']." AND `ptype`='".$arrows->fields['ptype']."'");
+	    $test = $db -> Execute("SELECT id FROM equipment WHERE name='".$arrows->fields['name']."' AND status='U' AND owner=".$player->id." AND power=".$arrows->fields['power']." AND poison=".$arrows->fields['poison']." AND `ptype`='".$arrows->fields['ptype']."' AND `cost`=".$arrows->fields['cost']) or die($db->ErrorMsg());
 	    if (!isset($test -> fields['id'])) 
 	      {
 		$db -> Execute("UPDATE `equipment` SET `status`='U' WHERE `type`='R' AND `owner`=".$player -> id." AND `status`='E'");
@@ -343,6 +343,7 @@ function equip ($id)
 	      {
 		$db -> Execute("UPDATE `equipment` SET `wt`=`wt`+".$arrows->fields['wt']." WHERE `id`=".$test -> fields['id']);
 		$db -> Execute("DELETE FROM `equipment` WHERE `id`=".$arrows->fields['id']);
+		$equip->fields['wt'] += $arrows->fields['wt'];
 	      }
 	    $test->Close();
 	  }
