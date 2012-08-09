921ceed89cc2aec1040a1b521ef046b857f1a0a0
thindil <thindil@tuxfamily.org>
2012-08-09 11:14:53 +0200

    update for text selection

--- js/editor.js
+++ js/editor.js
@@ -6,7 +6,7 @@
  *   @copyright            : (C) 2012 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@vallheru.net>
  *   @version              : 1.6
- *   @since                : 25.06.2012
+ *   @since                : 09.08.2012
  *
  */
 
@@ -28,26 +28,26 @@
 //
 // $Id$
 
-function insertAtCursor(myField, myValue, intPos) 
+function insertAtCursor(myField, openTag, endTag, intPos) 
 {
     //IE support
     myField.focus();
     if (document.selection) 
     {
 	sel = document.selection.createRange();
-	sel.text = myValue;
+	sel.text = openTag + sel.text + endTag;
     }
     //MOZILLA/NETSCAPE support
     else if (myField.selectionStart || myField.selectionStart == '0') 
     {
 	var startPos = myField.selectionStart;
 	var endPos = myField.selectionEnd;
-	myField.value = myField.value.substring(0, startPos) + myValue + myField.value.substring(endPos, myField.value.length);
+	myField.value = myField.value.substring(0, startPos) + openTag + myField.value.substring(startPos, endPos) + endTag + myField.value.substring(endPos, myField.value.length);
 	myField.setSelectionRange(startPos + intPos, startPos + intPos);
     } 
     else 
     {
-	myField.value += myValue;
+	myField.value += openTag + endTag;
     }
     
 }
@@ -57,27 +57,27 @@ function formatText(button, frmname, frmfield)
     switch (button)
     {
     case "bold":
-	insertAtCursor(document.forms[frmname].elements[frmfield], "[b][/b]", 3);
+	insertAtCursor(document.forms[frmname].elements[frmfield], "[b]", "[/b]", 3);
 	break;
     case "italic":
-	insertAtCursor(document.forms[frmname].elements[frmfield], "[i][/i]", 3);
+	insertAtCursor(document.forms[frmname].elements[frmfield], "[i]", "[/i]", 3);
 	break;
     case "underline":
-	insertAtCursor(document.forms[frmname].elements[frmfield], "[u][/u]", 3);
+	insertAtCursor(document.forms[frmname].elements[frmfield], "[u]", "[/u]", 3);
 	break;
     case "emote":
-	insertAtCursor(document.forms[frmname].elements[frmfield], "[b]**[/b]", 4);
+	insertAtCursor(document.forms[frmname].elements[frmfield], "[b]*", "*[/b]", 4);
 	break;
     case "center":
-	insertAtCursor(document.forms[frmname].elements[frmfield], "[center][/center]", 8);
+	insertAtCursor(document.forms[frmname].elements[frmfield], "[center]", "[/center]", 8);
 	break;
     case "quote":
-	insertAtCursor(document.forms[frmname].elements[frmfield], "[quote][/quote]", 7);
+	insertAtCursor(document.forms[frmname].elements[frmfield], "[quote]", "[/quote]", 7);
 	break;
     case "color":
 	var color = document.forms[frmname].elements["colors"].value;
 	var opentag = "[color " + color + "]";
-	insertAtCursor(document.forms[frmname].elements[frmfield], opentag + "[/color]", opentag.length);
+	insertAtCursor(document.forms[frmname].elements[frmfield], opentag, "[/color]", opentag.length);
 	break;
     default:
 	break;
