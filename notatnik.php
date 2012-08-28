120bed4598d5118627e2705a78a8c87c44c5b4bb
thindil <thindil@tuxfamily.org>
2012-08-28 10:41:44 +0200

    some redesign and code cleanup

--- notatnik.php
+++ notatnik.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2011,2012 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@vallheru.net>
  *   @version              : 1.6
- *   @since                : 04.07.2012
+ *   @since                : 28.08.2012
  *
  */
 
@@ -32,126 +32,131 @@
 $title = "Notatnik";
 require_once("includes/head.php");
 
-/**
-* Get the localization for game
-*/
-require_once("languages/".$lang."/notatnik.php");
 
-
-/**
- * Delete post
- */
-if (isset ($_GET['akcja']) && $_GET['akcja'] == 'skasuj') 
-{
-    checkvalue($_GET['nid']);
-    $did = $db -> Execute("SELECT `id`, `gracz` FROM `notatnik` WHERE `id`=".$_GET['nid']);
-    if (!$did -> fields['id']) 
-      {
-        message('error', NO_TEXT);
-      }
-    elseif ($player -> id != $did -> fields['gracz']) 
-      {
-        message('error', NOT_YOUR);
-      }
-    else
-      {
-	$db -> Execute("DELETE FROM `notatnik` WHERE `gracz`=".$player -> id." AND `id`=".$_GET['nid']);
-	message('success', YOU_DELETE);
-      }
-    unset($_GET['akcja']);
-}
-
-if (isset($_GET['akcja']) && ($_GET['akcja'] == 'dodaj' || $_GET['akcja'] == 'edit'))
+if (isset($_GET['akcja']))
   {
-    $smarty -> assign(array("Ttitle" => 'Tytuł:',
-			    "Note" => NOTE,
-                            "Asave" => A_SAVE,
-			    "Abold" => "Pogrubienie",
-			    "Aitalic" => "Kursywa",
-			    "Aunderline" => "Podkreślenie",
-			    "Aemote" => "Emocje/Czynność",
-			    "Ocolors" => array("red" => "czerwony",
-					       "green" => "zielony",
-					       "white" => "biały",
-					       "yellow" => "żółty",
-					       "blue" => "niebieski",
-					       "aqua" => "cyjan",
-					       "fuchsia" => "fuksja",
-					       "grey" => "szary",
-					       "lime" => "limonka",
-					       "maroon" => "wiśniowy",
-					       "navy" => "granatowy",
-					       "olive" => "oliwkowy",
-					       "purple" => "purpurowy",
-					       "silver" => "srebrny",
-					       "teal" => "morski"),
-			    "Acolor" => "Kolor",
-			    "Acenter" => "Wycentrowanie",
-			    "Aquote" => "Cytat"));
+    /**
+     * Delete post
+     */
+    if ($_GET['akcja'] == 'skasuj') 
+      {
+	checkvalue($_GET['nid']);
+	$did = $db -> Execute("SELECT `id` FROM `notatnik` WHERE `id`=".$_GET['nid']." AND `gracz`=".$player->id);
+	if (!$did -> fields['id']) 
+	  {
+	    message('error', "Nie ma takiego wpisu!");
+	  }
+	else
+	  {
+	    $db -> Execute("DELETE FROM `notatnik` WHERE `gracz`=".$player -> id." AND `id`=".$_GET['nid']);
+	    message('success', "Skasowałeś wpis.");
+	  }
+	unset($_GET['akcja']);
+      }
+
+    if (($_GET['akcja'] == 'dodaj' || $_GET['akcja'] == 'edit'))
+      {
+	$smarty -> assign(array("Ttitle" => 'Tytuł:',
+				"Note" => "Notatka",
+				"Asave" => "Zapisz",
+				"Abold" => "Pogrubienie",
+				"Aitalic" => "Kursywa",
+				"Aunderline" => "Podkreślenie",
+				"Aemote" => "Emocje/Czynność",
+				"Ocolors" => array("red" => "czerwony",
+						   "green" => "zielony",
+						   "white" => "biały",
+						   "yellow" => "żółty",
+						   "blue" => "niebieski",
+						   "aqua" => "cyjan",
+						   "fuchsia" => "fuksja",
+						   "grey" => "szary",
+						   "lime" => "limonka",
+						   "maroon" => "wiśniowy",
+						   "navy" => "granatowy",
+						   "olive" => "oliwkowy",
+						   "purple" => "purpurowy",
+						   "silver" => "srebrny",
+						   "teal" => "morski"),
+				"Acolor" => "Kolor",
+				"Acenter" => "Wycentrowanie",
+				"Aquote" => "Cytat"));
+      }
+
+    /**
+     * Add post
+     */
+    if ($_GET['akcja'] == 'dodaj') 
+      {
+	$smarty -> assign(array("Nlink" => "dodaj&amp;step=send",
+				"Ntext" => '',
+				"Ntitle" => ''));
+	if (isset ($_GET['step']) && $_GET['step'] == 'send') 
+	  {
+	    if (empty ($_POST['body']) || empty($_POST['title'])) 
+	      {
+		message('error', "Podaj tytuł oraz treść notatki.");
+		$smarty->assign(array("Ntext" => $_POST['body'],
+				      "Ntitle" => $_POST['title']));
+	      }
+	    else
+	      {
+		require_once('includes/bbcode.php');
+		$_POST['body'] = bbcodetohtml($_POST['body']);
+		$strBody = $db -> qstr($_POST['body'], get_magic_quotes_gpc());
+		$strDate = $db -> DBDate($newdate);
+		$_POST['title'] = htmlspecialchars($_POST['title'], ENT_QUOTES);
+		$db -> Execute("INSERT INTO `notatnik` (`gracz`, `tekst`, `czas`, `title`) VALUES(".$player -> id.", ".$strBody.", ".$strDate.", '".$_POST['title']."')");
+		message('success', "Notatka dodana.");
+		unset($_GET['akcja']);
+	      }
+	  }
+      }
+
+    /**
+     * Edit post
+     */
+    if ($_GET['akcja'] == 'edit')
+      {
+	checkvalue($_GET['nid']);
+	$objText = $db -> Execute("SELECT `id`, `tekst`, `title` FROM `notatnik` WHERE `id`=".$_GET['nid']." AND `gracz`=".$player->id);
+	if (!$objText -> fields['id']) 
+	  {
+	    message('error', "Nie ma takiego wpisu!");
+	    unset($_GET['akcja']);
+	  }
+	else
+	  {
+	    require_once('includes/bbcode.php');
+	    $strNbody = htmltobbcode($objText -> fields['tekst']);
+	    $smarty -> assign(array("Ntext" => $strNbody,
+				    "Ntitle" => $objText->fields['title'],
+				    "Nlink" => "edit&amp;nid=".$_GET['nid']."&amp;step=edit"));
+	  }
+	$objText -> Close();
+	if (isset($_GET['step']) && $_GET['step'] == 'edit') 
+	  {
+	    if (empty ($_POST['body']) || empty($_POST['title'])) 
+	      {
+		message('error', "Podaj tytuł oraz treść notatki.");
+		$smarty->assign(array("Ntext" => $_POST['body'],
+				      "Ntitle" => $_POST['title']));
+		
+	      }
+	    else
+	      {
+		$_POST['title'] = htmlspecialchars($_POST['title'], ENT_QUOTES);
+		require_once('includes/bbcode.php');
+		$_POST['body'] = bbcodetohtml($_POST['body']);
+		$strBody = $db -> qstr($_POST['body'], get_magic_quotes_gpc());
+		$strDate = $db -> DBDate($newdate);
+		$db -> Execute("UPDATE `notatnik` SET `tekst`=".$strBody.", `czas`=".$strDate.", `title`='".$_POST['title']."' WHERE `id`=".$_GET['nid']);
+		message('success', "Notatka zmieniona.");
+		unset($_GET['akcja']);
+	      }
+	  }
+      }
   }
-
-/**
- * Add post
- */
-if (isset ($_GET['akcja']) && $_GET['akcja'] == 'dodaj') 
-  {
-    $smarty -> assign(array("Nlink" => "dodaj&amp;step=send",
-                            "Ntext" => '',
-			    "Ntitle" => ''));
-    if (isset ($_GET['step']) && $_GET['step'] == 'send') 
-    {
-      if (empty ($_POST['body']) || empty($_POST['title'])) 
-        {
-            error ("Podaj tytuł oraz treść notatki.");
-        }
-        require_once('includes/bbcode.php');
-        $_POST['body'] = bbcodetohtml($_POST['body']);
-        $strBody = $db -> qstr($_POST['body'], get_magic_quotes_gpc());
-        $strDate = $db -> DBDate($newdate);
-	$_POST['title'] = htmlspecialchars($_POST['title'], ENT_QUOTES);
-        $db -> Execute("INSERT INTO `notatnik` (`gracz`, `tekst`, `czas`, `title`) VALUES(".$player -> id.", ".$strBody.", ".$strDate.", '".$_POST['title']."')");
-        message('success', YOU_ADD);
-	unset($_GET['akcja']);
-    }
-}
-
-/**
- * Edit post
- */
-if (isset($_GET['akcja']) && $_GET['akcja'] == 'edit')
-{
-    checkvalue($_GET['nid']);
-    $objText = $db -> Execute("SELECT `id`, `gracz`, `tekst`, `title` FROM `notatnik` WHERE `id`=".$_GET['nid']);
-    if (!$objText -> fields['id']) 
-    {
-        error(NO_TEXT);
-    }
-    if ($player -> id != $objText -> fields['gracz']) 
-    {
-        error(NOT_YOUR);
-    }
-    require_once('includes/bbcode.php');
-    $strNbody = htmltobbcode($objText -> fields['tekst']);
-    $smarty -> assign(array("Ntext" => $strNbody,
-			    "Ntitle" => $objText->fields['title'],
-                            "Nlink" => "edit&amp;nid=".$_GET['nid']."&amp;step=edit"));
-    $objText -> Close();
-    if (isset($_GET['step']) && $_GET['step'] == 'edit') 
-    {
-        if (empty ($_POST['body']) || empty($_POST['title'])) 
-        {
-            error("Podaj tytuł oraz treść notatki.");
-        }
-	$_POST['title'] = htmlspecialchars($_POST['title'], ENT_QUOTES);
-        require_once('includes/bbcode.php');
-        $_POST['body'] = bbcodetohtml($_POST['body']);
-        $strBody = $db -> qstr($_POST['body'], get_magic_quotes_gpc());
-        $strDate = $db -> DBDate($newdate);
-        $db -> Execute("UPDATE `notatnik` SET `tekst`=".$strBody.", `czas`=".$strDate.", `title`='".$_POST['title']."' WHERE `id`=".$_GET['nid']);
-        message('success', YOU_EDIT);
-	unset($_GET['akcja']);
-    }
-}
 
 if (!isset($_GET['akcja']))
   {
@@ -179,24 +184,18 @@ if (!isset($_GET['akcja']))
     
     $arrNotes = $db->GetAll("SELECT `id`, `tekst`, `czas`, `title` FROM `notatnik` WHERE `gracz`=".$player -> id." ORDER BY `id` DESC LIMIT ".(25 * ($intPage - 1)).", 25");
     $smarty -> assign(array("Notes" => $arrNotes,
-			    "Notesinfo" => NOTES_INFO,
-			    "Ntime" => N_TIME,
-			    "Adelete" => A_DELETE,
-			    "Aadd" => A_ADD,
-			    "Aedit" => A_EDIT,
+			    "Notesinfo" => "Tutaj możesz zapisywać sobie różne przydatne informacje.",
+			    "Notesinfo2" => "Oto lista notatek:",
+			    "Ntime" => "Czas",
+			    "Adelete" => "skasuj",
+			    "Aadd" => "Dodaj wpis",
+			    "Aedit" => "edytuj",
 			    "Tpages" => $intPages,
 			    "Tpage" => $intPage,
 			    "Fpage" => "Idź do strony:",
 			    "Checked" => $strChecked));
-  }
-
-/**
-* Initialization of variable
-*/
-if (!isset($_GET['akcja'])) 
-{
     $_GET['akcja'] = '';
-}
+  }
 
 /**
 * Assign variable to template and display page
--- templates/notatnik.tpl
+++ templates/notatnik.tpl
@@ -1,5 +1,7 @@
 {if $Action == ""}
     {$Notesinfo}<br /><br />
+    <a href="notatnik.php?akcja=dodaj">({$Aadd})</a><br /><br />
+    {$Notesinfo2}<br /><br />
     {foreach $Notes as $Note}
         <label for="mytoggle{$Note@index}" class="toggle">&#171; {$Note.title} &#187;</label> [<a href="notatnik.php?akcja=skasuj&amp;nid={$Note.id}">{$Adelete}</a> | <a href="notatnik.php?akcja=edit&amp;nid={$Note.id}">{$Aedit}</a>]
         <input id="mytoggle{$Note@index}" type="checkbox" class="toggle" {$Checked} />
@@ -23,18 +25,16 @@
 {if $Action == "edit" || $Action == "dodaj"}
     <script src="js/editor.js"></script>
     <form method="post" action="notatnik.php?akcja={$Nlink}" name="notes">
-    <table>
-    <tr><td>{$Ttitle}</td><td><input type="text" name="title" value="{$Ntitle}"></td></tr>
-    <tr><td colspan="2"><input id="bold" type="button" value="{$Abold}" onClick="formatText(this.id, 'notes', 'body')" />
+    <div align="center">{$Ttitle} <input type="text" name="title" value="{$Ntitle}" size="40" /><br /><br />
+        <input id="bold" type="button" value="{$Abold}" onClick="formatText(this.id, 'notes', 'body')" />
 	<input id="italic" type="button" value="{$Aitalic}" onClick="formatText(this.id, 'notes', 'body')" />
 	<input id="underline" type="button" value="{$Aunderline}" onClick="formatText(this.id, 'notes', 'body')" />
-	<input id="emote" type="button" value="{$Aemote}" onClick="formatText(this.id, 'notes', 'body')" />
+	<input id="emote" type="button" value="{$Aemote}" onClick="formatText(this.id, 'notes', 'body')" /><br />
 	<input id="center" type="button" value="{$Acenter}" onClick="formatText(this.id, 'notes', 'body')" />
 	<input id="quote" type="button" value="{$Aquote}" onClick="formatText(this.id, 'notes', 'body')" />
-	<input id="color" type="button" value="{$Acolor}" onClick="formatText(this.id, 'notes', 'body')" /> {html_options name=colors options=$Ocolors}</td></tr>
-    <tr><td valign="top">{$Note}:</td><td><textarea name="body" rows="20" cols="40">{$Ntext}</textarea></td></tr>
-    <tr><td colspan="2" align="center"><input type="submit" value="{$Asave}" /></td></tr>
-    </table>
+	<input id="color" type="button" value="{$Acolor}" onClick="formatText(this.id, 'notes', 'body')" /> {html_options name=colors options=$Ocolors}<br /><br />
+    {$Note}: <textarea name="body" rows="20" cols="40">{$Ntext}</textarea><br />
+    <input type="submit" value="{$Asave}" /></div>
     </form><br /><br />
     (<a href="notatnik.php">{$Aback}</a>)
 {/if}
