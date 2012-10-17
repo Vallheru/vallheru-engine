767eed8584ec15732aeb5c062aaa874471a0c08e
thindil <thindil@vallheru.net>
2012-10-17 10:30:06 +0200

    updated mines in city

--- mines.php
+++ mines.php
@@ -6,8 +6,8 @@
  *   @name                 : mines.php
  *   @copyright            : (C) 2004,2005,2006,2011,2012 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@vallheru.net>
- *   @version              : 1.6
- *   @since                : 22.08.2012
+ *   @version              : 1.7
+ *   @since                : 17.10.2012
  *
  */
 
@@ -32,14 +32,9 @@
 $title = "Kopalnie";
 require_once("includes/head.php");
 
-/**
-* Get the localization for game
-*/
-require_once("languages/".$lang."/mines.php");
-
 if ($player -> location != 'Altara')
 {
-    error (ERROR);
+    error ("Nie znajdujesz się w mieście.");
 }
 
 $objMines = $db -> Execute("SELECT `owner`, `copper`, `zinc`, `tin`, `iron`, `coal` FROM `mines` WHERE `owner`=".$player -> id);
@@ -65,29 +60,29 @@ if (!isset($_GET['mine']))
     $objSearch = $db -> Execute("SELECT `mineral`, `days` FROM `mines_search` WHERE `player`=".$player -> id);
     if ($objSearch -> fields['mineral'])
     {
-        $arrMinname = array(M_COPPER, M_ZINC, M_TIN, M_IRON, M_COAL);
+        $arrMinname = array("miedzi", "cynku", "cyny", "żelaza", "węgla");
         $intKey = array_search($objSearch -> fields['mineral'], $arrMinerals);
         if ($objSearch -> fields['days'] > 1)
         {
-            $strDays = SEARCH_DAYS;
+            $strDays = " dni.";
         }
             else
         {
-            $strDays = SEARCH_DAY;
+            $strDays = " dzień.";
         }
-        $strSearch = YOU_SEARCH.$arrMinname[$intKey].THIS_TAKE.$objSearch -> fields['days'].$strDays;
+        $strSearch = "Twój geolog aktualnie poszukuje złóż ".$arrMinname[$intKey].". Zajmie mu to jeszcze ".$objSearch -> fields['days'].$strDays;
     }
     else
     {
         $strSearch = '';
     }
     $objSearch -> Close();
-    $smarty -> assign(array("Minesinfo" => MINES_INFO,
-                            "Acopper" => A_COPPER,
-                            "Azinc" => A_ZINC,
-                            "Atin" => A_TIN,
-                            "Airon" => A_IRON,
-                            "Acoal" => A_COAL,
+    $smarty -> assign(array("Minesinfo" => "Witaj w kopalniach. Możesz tutaj wydobywać różne minerały. Poniżej znajdują się drogowskazy do odpowiednich kopalń",
+                            "Acopper" => "Kopalnia miedzi - złoża: ",
+                            "Azinc" => "Kopalnia cynku - złoża: ",
+                            "Atin" => "Kopalnia cyny - złoża: ",
+                            "Airon" => "Kopalnia żelaza - złoża: ",
+                            "Acoal" => "Kopalnia węgla - złoża: ",
                             "Minamount" => $arrMineralamount,
                             "Minessearch" => $strSearch));
 }
@@ -100,9 +95,9 @@ if (isset($_GET['mine']) && !isset($_GET['step']))
     $arrMines = array('copper', 'zinc', 'tin', 'iron', 'coal');
     if (!in_array($_GET['mine'], $arrMines))
     {
-        error(ERROR);
+        error("Zapomnij o tym.");
     }
-    $arrMinerals = array(M_COPPER, M_ZINC, M_TIN, M_IRON, M_COAL);
+    $arrMinerals = array("miedzi", "cynku", "cyny", "żelaza", "węgla");
     $intKey = array_search($_GET['mine'], $arrMines);
     if ($objMines -> fields[$_GET['mine']] > 0)
     {
@@ -112,11 +107,11 @@ if (isset($_GET['mine']) && !isset($_GET['step']))
     {
         $smarty -> assign("Mine2", 'Y');
     }
-    $smarty -> assign(array("Mineinfo" => MINE_INFO,
+    $smarty -> assign(array("Mineinfo" => "Witaj w kopalni ",
                             "Minename" => $arrMinerals[$intKey],
-                            "Asearch" => A_SEARCH,
-                            "Adig" => A_DIG,
-                            "Mineinfo2" => MINE_INFO2));
+                            "Asearch" => "Szukaj złóż",
+                            "Adig" => "Wydobywaj złoża",
+                            "Mineinfo2" => ". Oto co możesz tutaj zrobić:"));
 }
 
 /**
@@ -129,19 +124,19 @@ if (isset($_GET['step']) && $_GET['step'] == 'search')
     {
         if ($objTest -> fields['days'] > 1)
         {
-            $strDays = SEARCH_DAYS;
+            $strDays = " dni.";
         }
             else
         {
-            $strDays = SEARCH_DAY;
+            $strDays = " dzień.";
         }
-        error(YOU_SEARCH.$objTest -> fields['days'].$strDays);
+        error("Nie możesz szukać złóż, ponieważ twój geolog jest zajęty jeszcze przez ".$objTest -> fields['days'].$strDays);
     }
     $objTest -> Close();
     $arrMines = array('coal', 'copper', 'zinc', 'tin', 'iron');
     if (!in_array($_GET['mine'], $arrMines))
     {
-        error(ERROR);
+        error("Zapomnij o tym.");
     }
     $arrMinerals = array('coal' => 0.75,
                          'copper' => 1, 
@@ -167,12 +162,12 @@ if (isset($_GET['step']) && $_GET['step'] == 'search')
                                 "Smallgold" => $intSmallgold,
                                 "Mediumgold" => $intMediumgold,
                                 "Largegold" => $intLargegold,
-                                "Asearch" => A_SEARCH,
-                                "Minerals" => MINERALS,
-                                "Goldcoins" => GOLD_COINS,
-                                "Mithrilcoins" => MITHRIL_COINS,
-                                "Searchdays" => SEARCH_DAYS,
-                                "Searchday" => SEARCH_DAY,
+                                "Asearch" => "Szukaj",
+                                "Minerals" => "minerałów za",
+                                "Goldcoins" => "sztuk złota. Czas poszukiwań: ",
+                                "Mithrilcoins" => "sztuk mithrilu oraz",
+                                "Searchdays" => " dni.",
+                                "Searchday" => " dzień.",
                                 "Type" => $_GET['type']));
     }
         else
@@ -180,7 +175,7 @@ if (isset($_GET['step']) && $_GET['step'] == 'search')
         $arrType = array('small', 'medium', 'large');
         if (!in_array($_GET['type'], $arrType))
         {
-            error(ERROR);
+            error("Zapomnij o tym.");
         }
         if ($_GET['type'] == 'small')
         {
@@ -202,16 +197,16 @@ if (isset($_GET['step']) && $_GET['step'] == 'search')
         }
         if ($intGoldcost > $player -> credits)
         {
-            error(NO_MONEY);
+            error("Nie masz tyle sztuk złota!");
         }
         if ($intMithcost > $player -> platinum)
         {
-            error(NO_MITH);
+            error("Nie masz tyle sztuk mithrilu!");
         }
         $db -> Execute("UPDATE players SET credits=credits-".$intGoldcost.", platinum=platinum-".$intMithcost." WHERE id=".$player -> id);
         $db -> Execute("INSERT INTO mines_search (player, days, mineral, searchdays) VALUES(".$player -> id.", ".$intDays.", '".$_GET['mine']."', ".$intDays.")");
         $smarty -> assign(array("Type" => $_GET['type'],
-            "Message" => YOU_START));
+            "Message" => "Wydałeś geologowi polecenie poszukiwania złóż minerałów"));
     }
 }
 
@@ -223,38 +218,38 @@ if (isset($_GET['step']) && $_GET['step'] == 'dig')
     $arrMines = array('copper', 'zinc', 'tin', 'iron', 'coal');
     if (!in_array($_GET['mine'], $arrMines))
     {
-        error(ERROR);
+        error("Zapomnij o tym.");
     }
     if (!$objMines -> fields[$_GET['mine']])
     {
-        error(NO_MINERALS);
+        error("Nie ma złóż w tej kopalni!");
     }
     if (!isset($_POST['amount']))
       {
 	$_POST['amount'] = '';
       }
-    $arrMinerals = array(M_COPPER, M_ZINC, M_TIN, M_IRON, M_COAL);
+    $arrMinerals = array("rudy miedzi", "rudy cynku", "rudy cyny", "rudy żelaza", "węgla");
     $intKey = array_search($_GET['mine'], $arrMines);
-    $smarty -> assign(array("Yousend" => YOU_SEND,
+    $smarty -> assign(array("Yousend" => "Przeznacz na wydobycie",
                             "Minname" => $arrMinerals[$intKey],
-                            "Adig" => A_DIG,
-                            "Menergy" => M_ENERGY,
+                            "Adig" => "Wydobywaj",
+                            "Menergy" => " energii.",
 			    "Amount" => $_POST['amount'],
                             "Message" => ''));
     if (isset($_GET['dig']) && $_GET['dig'] == 'Y')
     {
         if (!isset($_POST['amount']))
         {
-            error(ERROR);
+            error("Zapomnij o tym.");
         }
 	checkvalue($_POST['amount']);
         if ($_POST['amount'] > $player -> energy)
         {
-            error(NO_ENERGY);
+            error("Nie masz tyle energii!");
         }
         if ($player -> hp < 1)
         {
-            error(YOU_DEAD);
+            error("Nie możesz kopać ponieważ jesteś martwy!");
         }
         $arrMinerals2 = array(1, 2, 3, 4, 1);
 
@@ -263,24 +258,24 @@ if (isset($_GET['step']) && $_GET['step'] == 'dig')
          */
 	$player->curskills(array('mining'), TRUE, TRUE);
 
-        $intAmount = ceil(((rand(1, 20) * $_POST['amount'] / $arrMinerals2[$intKey]) * (1 + $player->mining / 20)) - $arrMinerals2[$intKey]);
+        $intAmount = ceil(((rand(1, 20) * $_POST['amount'] / $arrMinerals2[$intKey]) * (($player->skills['mining'][1] + $player->stats['strength'][2]) / 20)) - $arrMinerals2[$intKey]);
+	$player->clearbless(array('strength'));
         if ($intAmount < 1)
         {
             $intAmount = 1;
         }
-        $intAbility = $_POST['amount'] / 10;
         if ($intAmount > $objMines -> fields[$_GET['mine']])
         {
             $intAmount = $objMines -> fields[$_GET['mine']];
         }
-	$intExp = ($intAmount * $arrMinerals2[$intKey]) / 4;
+	$intExp = ceil(($intAmount * $arrMinerals2[$intKey]) / 4);
 	if ($player->clas == 'Rzemieślnik')
 	  {
 	    $intExp = $intExp * 2;
 	  }
         $db -> Execute("UPDATE mines SET ".$_GET['mine']."=".$_GET['mine']."-".$intAmount." WHERE owner=".$player -> id);
-	require_once('includes/checkexp.php');
-	checkexp($player->exp, $intExp, $player->level, $player->race, $player->user, $player->id, 0, 0, $player->id, 'mining', $intAbility);
+	$player->checkexp(array('mining' => ($intExp / 2)), $player->id, 'skills');
+	$player->checkexp(array('strength' => ($intExp / 2)), $player->id, 'stats');
         $db -> Execute("UPDATE players SET energy=energy-".$_POST['amount'].", bless='', blessval=0 WHERE id=".$player -> id);
         $objTest = $db -> Execute("SELECT owner FROM minerals WHERE owner=".$player -> id);
         $arrOre = array('copperore', 'zincore', 'tinore', 'ironore', 'coal');
@@ -293,7 +288,7 @@ if (isset($_GET['step']) && $_GET['step'] == 'dig')
             $db -> Execute("UPDATE minerals SET ".$arrOre[$intKey]."=".$arrOre[$intKey]."+".$intAmount." WHERE owner=".$player -> id);
         }
         $objTest -> Close();
-        $smarty -> assign("Message", YOU_DIG.$_POST['amount'].M_ENERGY.' '.YOU_GET.$intAmount.T_AMOUNT.$arrMinerals[$intKey].', '.$intAbility.T_ABILITY2." oraz ".$intExp." PD.");
+        $smarty -> assign("Message", "Przeznaczyłeś na wydobycie ".$_POST['amount']." energii. Zdobyłeś w zamian ".$intAmount." sztuk ".$arrMinerals[$intKey]." oraz ".$intExp." PD.");
     }
 }
 
@@ -322,7 +317,7 @@ if (!isset($_GET['step']))
 */
 $smarty -> assign(array("Mine" => $_GET['mine'],
     "Step" => $_GET['step'],
-    "Aback" => A_BACK));
+    "Aback" => "Wróć"));
 $smarty -> display ('mines.tpl');
 
 require_once("includes/foot.php");
