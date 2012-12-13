7f4949e6a279f5bc8847243def5badf4bf9cabdd
thindil <thindil@vallheru.net>
2012-12-13 11:33:45 +0100

    added info about gained health

--- klasa.php
+++ klasa.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2012 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@vallheru.net>
  *   @version              : 1.7
- *   @since                : 10.10.2012
+ *   @since                : 13.12.2012
  *
  */
 
@@ -44,23 +44,23 @@ if (isset($_GET['klasa']))
       case 'wojownik':
 	$strClass = "Wojownicy są popularną klasa. W większości wojownikami zostają krasnoludy choć elfy i ludzie tez nie rezygnują z tej klasy. Mają największe zdolności bojowe - najważniejsze dla nich są siła i wytrzymałość ale tez bez odpowiedniej szybkości i zręczności się nie obejdzie. W Vallheru wojownicy stanowią największą część społeczeństwa. Podczas walki wojownik posługuje się bronią białą a najczęściej ubiera się w mocne zbroje - choć są w Vallheru znakomici szermierze którym zbroja tylko przeszkadza. Zostają złymi rzezimieszkami, silnymi ochroniarzami, wiernymi obrońcami, przebiegłymi złodziejami, lub po prostu neutralnymi poszukiwaczami przygód. Wojownikami zostają osoby które najczęściej szukają okazji do bitki i przetestowania swoich zdolności bojowych,
     lub z chęci zemsty na kimś ale tez dla samoobrony lub zmierzenia się na arenie z innymi dla chwały.<br />  Krasnoludy które zostają wojownikami są najczęściej nieokrzesani i porywczy, ale i silni oraz bardzo niebezpieczni, posługują się ciężka bronią i zbroja. Elfi szermierze posługują się lekka bronią i zbroja aby móc wykorzystać swoja zwinność podczas walki. Natomiast ludzie akceptujący tę profesję zostają równie dobrymi zbrojnymi co zwinnymi wojownikami.<br /> Cechy charakterystyczne wojownika <ul> <li>Premia do umięjętności atakujących oraz uników w wysokości 1/10 danej umiejętności</li><li>Możliwość używania ataków ofensywnych oraz defensywnych w walce turowej</li>";
-	$arrStats = array(1, 1, 1, 0, -1, 0);
+	$arrStats = array(1, 1, 1, 0, -1, 0, 5);
 	break;
       case 'mag':
 	$strClass = "Magowie maja swój stereotyp którego skrupulatnie się trzymają. Elfy najczęściej akceptują tą klasę choć ludzie nie wydają się być ta profesją mniej zainteresowane, natomiast krasnoludy krzywo na nią patrzą. Magów nie obchodzą bronie czy zbroje. Nie uważają iż siła czy wytrzymałość są najważniejsze - w sumie uważają je za zbędne. Siła maga są jego czary. W Vallheru magowie tworzą ścisłą elitę i zazwyczaj trzymają się razem. Są mądrzy i charyzmatyczni, inteligentni i roztropni - lecz są tez szaleni i nierozważni czarodzieje którzy wymyślają nowe dziwne zaklęcia. Magami zostają przeważnie osoby które chcą poznać tajniki wiedzy i potęgi magicznej aby mogli stać się najpotężniejszymi magami swych czasów.<br />Krasnoludy nie wybierają tej klasy chyba że są to jakieś bękarty - tacy zostają najczęściej szalonymi wynalazcami. Jest to ulubiona klasa elfów które są mądre i roztropne. Ludzcy magowie są inteligentni i utalentowani lecz znajdują się też wśród nich dziwni ludzie których nie powinno się dopuszczać do magii.<br />Cechy charakterystyczne Magów:<ul><li>Jako jedyni mogą używać czarów bojowych oraz obronnych</li><li>Mogą używać broni (ale nie jednocześnie z czarem bojowym) oraz nosić pancerze (chociaż te ograniczają ich czary - każdy poziom pancerza obniża siłę czarów maga o 1%)</li>";
-	$arrStats = array(0, 0, 0, 0, 1, 1);
+	$arrStats = array(0, 0, 0, 0, 1, 1, 3);
 	break;
       case 'craftsman':
 	$strClass = "Tę klasę tworzą rzemieślnicy, kowale, kolekcjonerzy chowańców i kupcy. Grupa ta nie ma określonych ulubionych ras którzy wybierają ta profesje. Każdy krasnolud, elf czy człowiek nadaje się na kupca czy kowala. Jednak tylko rzemieślnicy potrafią najzręczniej posługiwać się kowalskim młotem lub kupieckim sztucznym urokiem. Używają broni białej gdyż nie interesują ich zaklęcia - w zasadzie walka także lecz nigdy nie wiadomo z kim przyjdzie handlować wiec zawsze noszą przy sobie broń. Zostają bogatymi handlarzami, silnymi kowalami lub chciwymi szulerami lub przemytnikami. Rzemieślnikami zostają osoby którymi prowadzi chęć szybkiego zarobku a potem osiedlenie się i rozkręcanie interesu. Jednak niektórzy zarabiając nielegalnie pieniądze zakładają potajemne gildie złodziei lub przemytników.<br />Każda rasa ma takie same predyspozycje na rzemieślnika.<br />Cechy Rzemieślnika:<ul><li>Premia do umiejętności rzemieślniczych w wysokości 1/10 poziomu wybranej umiejętności</li><li>Kara 25% do zadawanych obrażeń w walce</li>";
-	$arrStats = array(0, 0, 0, 0, 0, 0);
+	$arrStats = array(0, 0, 0, 0, 0, 0, 2);
 	break;
       case 'barbarzynca':
 	$strClass = "<i>Byłeś wspaniałym przeciwnikiem! Wspomnę twe imię gdy podczas uczt będę pił najlepsze trunki z twej czaszki!</i><br />Gdyby wojownika nazwać synem wojny, barbarzyńcę trzeba by było nazwać jej mężem. Z dalekich i dzikich miejsc całego Vallheru u bram ".$city1b." pojawili się barbarzyńcy, głodni bogactw oraz sławy. Ich domem jest pole bitwy, ich językiem dźwięk wydawany przez broń w momencie ataku. Są najbardziej nieokrzesaną grupą obywateli wśród mieszkańców Vallheru. Nie cierpią magii ani jakiejkolwiek pomocy ze strony magów, uważają iż o chwale barbarzyńcy decyduje on sam a nie jakieś nieznane moce. Unikają przez to jakiejkolwiek formy magii czy to czarów czy magicznych broni. Jednak owa niechęć sprawia, iż są bardziej odporni na działania uroków niż inne kasty mieszkańców. Owe uprzedzenia do magów wywołały już kilka potężnych bitew z magami. Na ślady owych walk można natknąć się czasami, podróżując po bezdrożach Vallheru. Najwięcej barbarzyńców można spotkać pośród Jaszczuroludzi, najmniej - pośród Hobbitów. Ich główną domeną jest walka, lecz podobnie jak wojownicy jako tako radzą sobie również z kowalstwem czy stolarstwem. Niezależnie czy barbarzyńcą jest Krasnolud czy Człowiek, kasta ta ma takie same cechy<br />Cechy Barbarzyńcy<ul><li>Premia do umiejętności ataku oraz uniku w wysokości 1/10 danej umiejętności</li><li>Premia do odporności na magię w wysokości 1/2 poziomu siły woli</li><li>Możesz używać dwóch broni jednoręcznych na raz</li><li>Barbarzyńcy nie mogą używać ani czarów ani przedmiotów magicznych - mogą nosić je w plecaku ale nie mogą ich zakładać</li><li>W trakcie walki turowej mogą używać ataków ofensywnych";
-	$arrStats = array(1, 1, 1, 0, -1, 1);
+	$arrStats = array(1, 1, 1, 0, -1, 1, 5);
 	break;
       case 'zlodziej':
 	$strClass = "<i>Hej, przyjacielu! Potrzebujesz może pomocy? Mogę ci pokazać parę ciekawych miejsc w naszym pięknym mieście! Choć pokażę ci skrót, proszę tą wąską uliczką. Kim są ci uzbrojeni osobnicy? To moi wspólnicy, razem prowadzimy interesy...</i><br /> Mówi się, że kiedy pojawiła się inteligencja pojawili się i oni - Złodzieje. Ponoć to jeden z najstarszych zawodów świata, jednak na pewno jeden z mniej poważanych. Nikt nie lubi złodziei ale każdy chciałby mieć ich po swojej stronie - z jeszcze innej strony zaprzyjaźnienie się z kimś takim to jak zaprzyjaźnienie się z kobrą. Jedno jest pewne - już nie będziesz się nudził więcej w towarzystwie złodzieja. Złodzieje to przede wszystkim sprytni osobnicy, którzy ponad siłę i otwartą, honorową walkę stawiają podstępy oraz oszustwo. Kiedy mogą, unikają walki, lecz gdy już do niej staną starają się użyć wszelkich sposobów aby wygrać. Niestety to wszystko powoduje iż nie są zbyt lubiani pośród mieszkańców Vallheru, a już na pewno siły porządkowe próbują zwalczać ich na każdym kroku. Wielu z nich prowadzi podwójne życie, tak aby zabezpieczyć się na wypadek gdyby coś poszło nie tak. Lecz nawet w tej wydawałoby się do końca zepsutej kaście, czasami natknąć można się na kogoś kto wspiera innych bez oglądania się na własne korzyści. Tacy najczęściej zostają zwiadowcami karawan, czy też przewodnikami grup poszukiwaczy przygów. Jednego można się spodziewać po złodzieju - tego że nic nie jest pewne. Najczęściej tę ścieżkę życiową wybierają Hobbici, choć słyszano również o Jaszczuroludziach którzy pałali się tą profesją. Pewne cechy są niezależne od rasy danej postaci.<br />Cechy Złodzieja:<ul><li>Możliwość okradania banku, gracza lub sklepu</li><li>Możliwość szpiegowania klanów raz na reset</li><li>Możliwość sabotowania budowy Astralnej Machiny raz na reset</li><li>Jeżeli złodziej dokona nieudanej próby kradzieży (zostanie złapany) - trafia do lochów na jakiś czas</li>";
-	$arrStats = array(0, 1, -1, 1, 1, -1);
+	$arrStats = array(0, 1, -1, 1, 1, -1, 4);
 	break;
       default:
 	error('Zapomnij o tym.');
@@ -107,7 +107,7 @@ if (isset($_GET['klasa']))
 	error ("<br />Wybrałeś ".$strClas2.". Kliknij <a href=\"stats.php\">tutaj</a> aby wrócić.");
       }
     $smarty->assign(array("Classinfo" => $strClass,
-			  "Tstats" => array('do siły', 'do zręczności', 'do kondycji', 'do szybkości', 'do inteligencji', 'do siły woli'),
+			  "Tstats" => array('do siły', 'do zręczności', 'do kondycji', 'do szybkości', 'do inteligencji', 'do siły woli', 'punktów życia za każdy zdobyty poziom Kondycji'),
 			  "Stats" => $arrStats));
   }
 else
--- rasa.php
+++ rasa.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2012 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@vallheru.net>
  *   @version              : 1.7
- *   @since                : 10.10.2012
+ *   @since                : 13.12.2012
  *
  */
 
@@ -43,33 +43,33 @@ if (isset($_GET['rasa']))
       {
       case 'czlowiek':
 	$strRaceinfo = "Ludzie są najbardziej zróżnicowaną i wszechstronną rasą. Są dość wysocy gdyż mierzą około 150 - 190 cm wzrostu lecz są osoby którzy są o wiele wyżsi ale i niżsi. Maja rożną posturę jednak najczęściej są umięśnieni i szczupli. Dzięki tej różnorodności wśród tej rasy mogą być oni wyćwiczeni w każdej dziedzinie. Wielu ludzkich magów pokonało niejednego elfiego maga oraz nie mniej wojowników pokonało wielu krasnoludzkich wojowników. Nie maja określonych najlepszych i najgorszych stron - we wszystkim są dobrzy ale tylko jeśli trenują jedna dziedzinę - inaczej są dobrzy, lecz nie najlepsi. Atutem ludzi jest ich wszechstronność. Najlepszym przykładem są zarówno potężni magowie jak i wojownicy. Należą oni do najbardziej zuchwałych i ambitnych ras. Najczęściej walczą dla bogactwa i sławy niźli dla osobistego kształtowania swych umiejętności. Ludzie jako rasa najlepiej przyswajają nową wiedze, toteż szybko staja się potężni. Nie są wrogo nastawieni do innych w przeciwieństwie do krasnoludów i elfów. Są także uznawani za najbardziej ekscentryczną z ras, znów z powodu ich różnorodności. Ludzie są najlepsza rasa gdyż potrafią przystosować się do każdego środowiska w szybkim czasie i dlatego tez szybko uczą się wybranej przez siebie profesji.";
-	$arrStats = array(3, 3, 3, 3, 3, 3);
-	$arrMaxstats = array(50, 50, 50, 50, 50, 50);
+	$arrStats = array(3, 3, 3, 3, 3, 3, 4);
+	$arrMaxstats = array(50, 50, 50, 50, 50, 50, '');
 	break;
       case 'elf':
 	$strRaceinfo = "Elfy to bardzo dziwne istoty. Są skryte i najczęściej tolerują tylko innych ze swej rasy. Są szczupłe i niskiego wzrostu bo około 135 - 165 cm wzrostu. Jednak mimo iż wyglądają na słabych maja niezrównane zdolności magiczne. Niewielu przeżywa spotkanie z doświadczonym i rozwścieczonym elfim magiem. Mimo iż są świetnymi magami również nie są dyskryminowane jako wojownicy. Ich finezja w walce porównywana jest do ruchów kota a siła do tygrysa. Zwinność jest największym atutem elfa ponieważ jako mag musi nie tylko znać magiczne formuły lecz wykonywać szybkie i zręczne magiczne gesty, a gdyby zwinność zawiodła go w walce bronią wojownika - byłby zgubiony. Nie przepadają za innymi rasami gdyż o każdej twierdzą co innego. Ludzie są dla nich nieobyci a krasnoludy zupełnie niezabawne. Podroż zaczynają głównie dla chęci wyruszenia i poznania świata. Najbardziej lubią ukazywać swa zdolność do zwinnej walki mieczem i łukiem, no i oczywiście posługiwaniem się magią. Są bardzo wyniosłe i eleganckie aczkolwiek aroganckie i nietolerancyjne. Elfy są najlepszą rasą gdyż ich atutem jest szybkość i zwinność dzięki których potrafią z niebywałą prędkością dopadać przeciwników.";
-	$arrStats = array(2, 4, 3, 3, 3, 3);
-	$arrMaxstats = array(40, 60, 50, 55, 50, 50);
+	$arrStats = array(2, 4, 3, 3, 3, 3, 3);
+	$arrMaxstats = array(40, 60, 50, 55, 50, 50, '');
 	break;
       case 'krasnolud':
 	$strRaceinfo = "Krasnoludy znane są ze swej ogromnej siły, zdolności bojowej oraz picia piwa w karczmie. Może nie zadziwiają nikogo wzrostem bo mierzą około 120 - 135 cm wzrostu - jednak nie należy ich przez to lekceważyć. Są bardzo krzepkie i umięśnione oraz każdy z nich nosi brodę (krasnolud nie mający brody uznawany jest za zhańbionego). Ich atutem jest siła - i oczywiście to oni są najlepszymi kowalami Krain. Podróżują głównie dla zdobycia chwały i szacunku dla swojego klanu. Żyją do 400 lat może nawet dłużej wiec maja dużo czasu na osiągniecie w sobie doskonałości w danej profesji. Maja dość neutralne relacje z ludźmi lecz co do elfów maja duże uprzedzenia. Mimo iż kiedyś elfy walczyły ramie w ramie z krasnoludami teraz czują do siebie dużą antypatię. Nie są powszechnie lubiana rasa gdyż są zadziorni i bardzo uparci. Są najlepszą rasa bo potrafią wykorzystać swoje największe umiejętności czyli siłę i wytrzymałość do walki skuteczniej niż inne rasy.";
-	$arrStats = array(4, 2, 4, 2, 3, 3);
-	$arrMaxstats = array(60, 40, 60, 40, 50, 50);
+	$arrStats = array(4, 2, 4, 2, 3, 3, 5);
+	$arrMaxstats = array(60, 40, 60, 40, 50, 50, '');
 	break;
       case 'hobbit':
 	$strRaceinfo = "Hobbici niedawno przybyli do Vallheru, lecz już zdążyli zaznaczyć swoją obecność. Znani są z pogodnego charakteru oraz umiejętności wyczyszczenia spiżarni w czasie krótszym niż legion wygłodniałych Krasnoludów. Są najniższą znaną rasą - mierzą od 90 do 120 cm wzrostu. Mają najczęście ciemnobrązowe, kędzierzawe włosy oraz brązowe oczy. Cechą charakterystyczną tej rasy jest fakt iż żaden z jej przedstawicieli nie nosi obuwia. Spowodowane jest to tym, że posiadają na stopach niesamowicie bujne owłosienie, dzięki któremu mogą na bosaka wędrować nawet po śniegu. Ogólnie przypominają Ludzi stąd zwykło nazywać się ich Małymi Ludźmi. Większość Hobbitów jest nieco otyła, co wprowadziło już w błąd wielu przedstawicieli innych ras, albowiem jeżeli Hobbit zechce może porusząć się równie zwinnie jak Elf. Są przy tym bardzo wytrzymali na trudy. Jednak rzadko owe cechy ujawniają się w tej rasie, ponieważ nad brud gościnca oraz dreszczyk przygód, rasa ta przedkłada ciepło domowego kominka. Jeżeli już wyruszają w świat, to raczej z czystej ciekawości niż ze względu na jakieś szczytne cele. Jest to rasa bardzo pokojowa, żyje z dala od spraw wielkiego świata i nie miesza się do sporów innych istot. Mało który z nich para się wojaczką czy magą ale ze względu na swoją pracowitość, przedmioty wykonane przez Hobbitów bez problemu mogą konkurować z dziełami mistrzów elfich czy też krasnoludzkich - przynajmniej jeżeli chodzi o wykonanie. Według niepotwierdzonych informacji wielu z podróżników tej rasy, którzy przybyli do ".$city1b." zasiliło szeregi przestępczej organizacji znanej jako Gildia Złodziei. Śledztwo w tej sprawie trwa.";
-	$arrStats = array(2, 4, 2, 4, 3, 3);
-	$arrMaxstats = array(40, 60, 45, 60, 50, 50);
+	$arrStats = array(2, 4, 2, 4, 3, 3, 4);
+	$arrMaxstats = array(40, 60, 45, 60, 50, 50, '');
 	break;
       case 'reptilion':
 	$strRaceinfo = "Jaszczuroludzie to jedna z najstarszych ras inteligentnych jakie znajdują się na ziemiach Vallheru. Dawni panowie tego świata, dziś już tylko cienie własnej potęgi sprzed lat. Obecnie jako nomadowie zamieszkują Pustynię Hansid el-Suda. Jako że najlepiej czują się w gorącym klimacie dopiero od niedawna spotkać można przedstawicieli tej rasy w ".$city1a.". Z wyglądu przypominają potężną dwunożną jaszczurkę. Najwyższy znany przedstawiciel tej rasy miał ok 2,5 metra wzrostu, najniższy - 150 cm. Ich skóra z biegiem czasu pokrywa się twardymi łuskami które mogą pełnić rolę dodatkowego pancerza. Najczęściej mają zielony lub brązowy kolor skóry. Jednak największą dumą każdego Reptiliona jest jego ogon - są bardzo przeczuleni na jego punkcie. Ich gadzie, pozbawione emocji twarze sprawiają iż przedstawiciele innych ras czują się w ich towarzystwie bardzo nieswojo. Większość Jaszczuroludzi jest niespokojna i agresywna - idealny materiał na wojownika. Znani są ze swej siły oraz szybkości. Jednak o ich inteligencji krążą wśród innych ras setki dowcipów. W swojej ojczyźnie praktycznie żaden Gadoczłek nie zajmuje się magią, jedynie odstępcy, którzy opóścili swych rodaków i przybyli do ".$city1b." próbują nią się zajmować, najczęściej jednak z mizernym skutkiem. Wielu z nich nie cierpi jakiejkolwiek formy magii. Ci co opuścili swoje klany to najczęsciej żadni bogactw i chwały awanturnicy. Niestety klimat Vallheru sprawia iż rasa ta na innych terenach niż pustynie nie może pokazać wszystkich swych możliwości. Jest to spowodowane tym iż Jaszczuroludzie są zmiennocieplni, stąd najlepiej czują się w bardzo gorącym klimacie - a tych jest niestety dość mało. Wyroby ich kowali czy też łuczników nie są może zbyt piękne ale za to bardzo funkcjonalne. Dawniej rasa ta była władcą tych ziem, jednak obecnie mało kto liczy się ze zdaniem jej przedstawicieli. Reptilioni sami odizolowalisię od innych ras kontaktując się najczęściej jedynie z przedstawicielami swojego gatunku. Większość z tych co przybywają do ".$city1b.", ze względu na swoją porywczość i nieokrzesanie nazywa się barbarzyńcami.";
-	$arrStats = array(4, 3, 3, 4, 2, 3);
-	$arrMaxstats = array(60, 50, 50, 60, 40, 50);
+	$arrStats = array(4, 3, 3, 4, 2, 3, 5);
+	$arrMaxstats = array(60, 50, 50, 60, 40, 50, '');
 	break;
       case 'gnome':
 	$strRaceinfo = "Gnomy pojawiły się na Vallheru całkiem niedawno. Złośliwi twierdzą iż są rasa ta jest efektem ubocznym jakiegoś zaklęcia. I rzeczywiście, ich odporność na magię jest nieco niższa niż u innych ras (może z wyjątkiem Jaszczuroludzi). Gnomy znane są z nieco chaotycznego charakteru, ich pęd do poznania całego świata nie raz wpędził przedstawiciela tej rasy (oraz jego przyjaciół) w kłopoty. Z zachowania nieco podobni do Hobbitów oraz Krasnoludów, lubują się przede wszystkim w rzemiośle oraz różnych dziwnych wynalazkach. Podekscytowany Gnom potrafi wyrzucać z siebie słowa szybciej niż najszybszy elfi łucznik strzały. Są jedną z najniższych znanych ras istot inteligentnych - mierzą od 100 do 130 cm wzrostu. Z budowy ciała podobni nieco do Krasnoludów (ci ostatni bardzo energicznie wypierają się jakichkolwiek porównań), o prostych jasnych włosach (rzecz jasna, jeżeli wcześniej dany osobnik nie stracił ich podczas eksperymentów alchemicznych) oraz czarnych oczach. Żywe usposobienie sprawia iż ciężko nie czuć sympatii do Gnomów - przynajmniej do ich pierwszego wybryku. Rzadko zajmują się magią - brak im cierpliwości do czegoś czego efektów nie widać od razu. Jako rasa pokojowo nastawiona, rzadko również zostają wojownikami - choć taki Gnom uzbrojony i wyposażony w najnowsze wynalazki gnomiej techniki potrafi siać prawdziwe spustoszenie na polu walki. Niestety również i wśród własnych towarzyszy. Ich zamiłowanie do rzemiosła oraz alchemii jest ogólnie znane. To ostatnie również bardzo widowiskowe. Wyroby ich rzemieślników są doskonałej jakości, choć czasami zbytnio udziwnione. Są przyjacielsko nastawieni do wszystkich znanych ras. Gnomy mają podwojoną premię z profesji Rzemieślnik.";
-	$arrStats = array(2, 4, 2, 3, 4, 2);
-	$arrMaxstats = array(40, 55, 40, 50, 55, 40);
+	$arrStats = array(2, 4, 2, 3, 4, 2, 2);
+	$arrMaxstats = array(40, 55, 40, 50, 55, 40, '');
 	break;
       default:
 	error('Zapomnij o tym');
@@ -127,7 +127,7 @@ if (isset($_GET['rasa']))
     $smarty->assign(array("Raceinfo" => $strRaceinfo,
 			  "Stats" => $arrStats,
 			  "Maxstats" => $arrMaxstats,
-			  "Tstats" => array('do siły, maksymalna wartość cechy:', 'do zręczności, maksymalna wartość cechy:', 'do kondycji, maksymalna wartość cechy:', 'do szybkości, maksymalna wartość cechy:', 'do inteligencji, maksymalna wartość cechy:', 'do siły woli, maksymalna wartość cechy:'),
+			  "Tstats" => array('do siły, maksymalna wartość cechy:', 'do zręczności, maksymalna wartość cechy:', 'do kondycji, maksymalna wartość cechy:', 'do szybkości, maksymalna wartość cechy:', 'do inteligencji, maksymalna wartość cechy:', 'do siły woli, maksymalna wartość cechy:', 'punktów życia za każdy zdobyty poziom cechy Kondycja'),
 			  "Aback" => "Wróć",
 			  "Aselect" => "Wybierz"));
   }
