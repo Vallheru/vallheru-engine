944e0219d76356948f024305bf2f741e8373507b
thindil <thindil@vallheru.net>
2012-12-17 10:21:58 +0100

    fixed typo

--- train.php
+++ train.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2007,2011,2012 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@vallheru.net>
  *   @version              : 1.6
- *   @since                : 11.10.2012
+ *   @since                : 17.12.2012
  *
  */
 
@@ -284,7 +284,7 @@ $smarty -> assign(array("Traininfo" => $strDesc,
 			"Toptions" => array("strength" => "Siłę",
 					   "agility" => "Zręczność",
 					   "speed" => "Szybkość",
-					   "condition" => "Wytrzymałość",
+					   "condition" => "Kondycję",
 					   "inteli" => "Inteligencję",
 					   "wisdom" => "Siłę Woli"),
                         "Iwant" => "Chcę trenować moją",
