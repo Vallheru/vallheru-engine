9a34c938ae8626dffa992eb68fc6b919d71917fd
thindil <thindil@vallheru.net>
2013-01-31 10:32:38 +0100

    fixed monster damage

--- includes/funkcje.php
+++ includes/funkcje.php
@@ -7,7 +7,7 @@
  *   @copyright            : (C) 2004,2005,2006,2007,2011,2012,2013 Vallheru Team based on Gamers-Fusion ver 2.5
  *   @author               : thindil <thindil@vallheru.net>
  *   @version              : 1.7
- *   @since                : 03.01.2013
+ *   @since                : 31.01.2013
  *
  */
 
@@ -579,6 +579,8 @@ function monsterattack2($intMydodge, &$zmeczenie, &$gunik, &$enemy, $times, $mcz
   global $player;
   global $smarty;
   global $number;
+  global $arrElements3;
+  global $arrElements4;
 
   $strMessage = '';
   //Player dodge
@@ -943,6 +945,8 @@ function fightmonster($enemy, $expgain, $goldgain, $times)
     global $number;
     global $arrTags;
     global $lang;
+    global $arrElements3;
+    global $arrElements4;
 
     $mczar = $db -> Execute("SELECT * FROM `czary` WHERE `status`='E' AND `gracz`=".$player -> id." AND `typ`='B'");
     $mczaro = $db -> Execute("SELECT * FROM `czary` WHERE `status`='E' AND `gracz`=".$player -> id." AND `typ`='O'");
